cb3e3c5ebf2d721a662192b686f39bc1fcc19cb8
Lệ Thu <nguyenthilethu2001vn@gmail.com>
2021-11-18 19:40:45 -0800

    devpro-website-tin-tuc

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$("docment").ready(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop()) {
+			$("#backtop").fadeIn();
+		} 
+		else {
+			$("#backtop").fadeOut();
+		}
+	});
+	$("#backtop").click(function(){
+		$('html, body').animate({
+			scrollTop: 0
+		}, 800);
+	});
+});
